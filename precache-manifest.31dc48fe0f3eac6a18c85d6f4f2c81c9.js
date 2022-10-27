865266cc16b2a47251bec2a15f77730ee4b31a09
Adebayo Samuel <eladebayoor@gmail.com>
2022-10-27 18:57:22 +0100

    Updates

---
+++ precache-manifest.31dc48fe0f3eac6a18c85d6f4f2c81c9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1cb4df75ca4973e36475",
+    "url": "/Adebayo-S/myReads-react-app/static/css/main.ab8bfb5b.chunk.css"
+  },
+  {
+    "revision": "1cb4df75ca4973e36475",
+    "url": "/Adebayo-S/myReads-react-app/static/js/main.1cb4df75.chunk.js"
+  },
+  {
+    "revision": "c9bc8df1e8dce40be5ed",
+    "url": "/Adebayo-S/myReads-react-app/static/js/1.c9bc8df1.chunk.js"
+  },
+  {
+    "revision": "780596714e723c4dd988",
+    "url": "/Adebayo-S/myReads-react-app/static/js/runtime~main.78059671.js"
+  },
+  {
+    "revision": "20e8847d1eb7759c19b0e30beb8849cf",
+    "url": "/Adebayo-S/myReads-react-app/static/media/arrow-back.20e8847d.svg"
+  },
+  {
+    "revision": "9d4e3f366f039249695de85a30a02b95",
+    "url": "/Adebayo-S/myReads-react-app/static/media/arrow-drop-down.9d4e3f36.svg"
+  },
+  {
+    "revision": "75d390e5eabc3254418e950df631389b",
+    "url": "/Adebayo-S/myReads-react-app/static/media/add.75d390e5.svg"
+  },
+  {
+    "revision": "91e56a375975e04529dcdc0f7a618958",
+    "url": "/Adebayo-S/myReads-react-app/index.html"
+  }
+];
